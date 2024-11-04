562f11b7fff666fea10c672eaa78085a712d83ed
Krishthakar <krishthakar198@gmail.com>
2024-11-03 21:55:50 -0500

    lab07

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
+
+
+import React from 'react';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
+  const studentInfo = {
+    name: "Krish Thakar",
+    id: "101413517",
+    college: "George Brown College, Toronto",
+    course: "React JS Programming Week09 Lab exercise",
+    title: "Welcome to Fullstack Development - I"
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{studentInfo.title}</h1>
+        <h2>{studentInfo.course}</h2>
+        <p>{studentInfo.id}</p>
+        <p>{studentInfo.name}</p>
+        <p>{studentInfo.college}</p>
       </header>
     </div>
   );
